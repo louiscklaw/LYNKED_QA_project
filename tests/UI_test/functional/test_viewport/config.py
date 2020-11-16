0b6ccfed2445c86cdc915e47765045ac267fc9ab
louiscklaw <louiscklaw@gmail.com>
2020-11-16 20:36:57 +0800

    update and test,

--- tests/UI_test/functional/test_viewport/config.py
+++ tests/UI_test/functional/test_viewport/config.py
@@ -4,4 +4,4 @@ from pprint import pprint
 SRC_DIR=os.path.dirname(__file__)
 PROJ_HOME=os.path.abspath(SRC_DIR+'/..')
 # SCREENCAPTURE_PATH=os.path.abspath(PROJ_HOME+'/screenshot')
-SCREENCAPTURE_PATH='/home/logic/_workspace/LYNKED_QA_project/reports/UI_test/functional/test_viewport'
+SCREENCAPTURE_PATH='reports/functional/test_viewport'
