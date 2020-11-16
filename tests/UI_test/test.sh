28fce618859cfb36154fdd1acdd95b24634d0a65
louiscklaw <louiscklaw@gmail.com>
2020-11-16 18:14:45 +0800

    update scripts,

--- tests/UI_test/test.sh
+++ tests/UI_test/test.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-pipenv run pytest tests/UI_test/functional/test_helloworld.py
-pipenv run pytest tests/UI_test/new_feature/test_helloworld.py
-pipenv run pytest tests/UI_test/regression/test_helloworld.py
+pipenv run pytest --html=docs/functional/report.html tests/UI_test/functional/test_helloworld.py
+pipenv run pytest --html=docs/new_feature/report.html tests/UI_test/new_feature/test_helloworld.py
+pipenv run pytest --html=docs/regression/report.html tests/UI_test/regression/test_helloworld.py
