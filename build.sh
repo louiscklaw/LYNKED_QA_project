2bbed1b8b0291699f9b40ee8589c3199c4578b40
louiscklaw <louiscklaw@gmail.com>
2020-11-16 20:05:55 +0800

    update,

--- build.sh
+++ build.sh
@@ -2,12 +2,28 @@
 
 set -ex
 
-REPO_HOME=$(dirname $0)
-PATH=$PATH:$REPO_HOME/drivers/chrome/85
+function shutdown {
+    ./scripts/down_docker_selenium.sh
+    echo "Shutdown complete"
+}
+
+trap shutdown SIGTERM SIGINT EXIT
+
+# export REPO_HOME=..
+# PATH=$PATH:$REPO_HOME/drivers/chrome/85
+
+rm -rf reports/* || true
+mkdir -p reports/functional/test_viewport
 
 sh docs/flows/test_site_flows/build.sh
 
-# pipenv run pytest --html=docs/functional/report.html tests
+./scripts/up_docker_selenium.sh
+
+echo 'sleep a while to let docker steady'
+sleep 30
+
+pipenv sync
+
 pipenv run pytest --html=reports/regression/report.html tests/UI_test/regression
 pipenv run pytest --html=reports/functional/report.html tests/UI_test/functional
 pipenv run pytest --html=reports/new_feature/report.html tests/UI_test/new_feature
--- tests/UI_test/functional/test_viewport/test_ViewPort.py
+++ tests/UI_test/functional/test_viewport/test_ViewPort.py
@@ -7,13 +7,28 @@ from viewports import *
 # from tests.UI_test.functional.test_viewport.config import SCREENCAPTURE_PATH
 from config import *
 
+SELENIUM_HUB_HOST='localhost'
+SCREENCAPTURE_PATH='/home/logic/_workspace/LYNKED_QA_project/reports/functional/test_viewport'
+
+def get_sc_filename(SCREENCAPTURE_PATH, viewport_name):
+  return '{}/{}_test.png'.format(SCREENCAPTURE_PATH,viewport_name.replace('/','_').replace(' ','_'))
+
 def test_ViewPort():
-  browser = webdriver.Chrome()
-  browser.get('http://www.google.com')
+  selenium_url = 'http://{}:4444/wd/hub'.format(SELENIUM_HUB_HOST)
+
+  browser = webdriver.Remote(
+    command_executor=selenium_url,
+    desired_capabilities = {
+      "browserName":"firefox",
+      "version":"",
+      "platform":"LINUX"
+      })
+
+  browser.get('https://www.google.com')
 
   for viewport_config in ALL_VIEWPORTS:
     (viewport_name, dump1, dump2 , width, height) = viewport_config
-    sc_filename = '{}/{}_test.png'.format(SCREENCAPTURE_PATH,viewport_name)
+    sc_filename =get_sc_filename(SCREENCAPTURE_PATH,viewport_name)
 
     browser.set_window_size(width, height)
     browser.save_screenshot(sc_filename)
