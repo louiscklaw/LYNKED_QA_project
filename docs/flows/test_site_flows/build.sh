1c92264644cefaaa8bcbc09895850a2b12397c6e
louiscklaw <louiscklaw@gmail.com>
2020-11-18 13:10:37 +0800

    update notes,

--- docs/flows/test_site_flows/build.sh
+++ docs/flows/test_site_flows/build.sh
@@ -10,4 +10,7 @@ export PATH=$PATH:$TEST_SITE_FLOWS_DIR/node_modules/.bin
 
 # mmdc -i happy_flow_1.mmd -o happy_flow_1.png
 # mmdc -i happy_flow_1.mmd -o happy_flow_1.png
+
+# npm install
+
 mmdc -i $FLOWS_DIR/user_flow/happy-flow-1.mmd -o $FLOWS_DIR/user_flow/happy-flow-1.png
---
+++ docs/flows/user_flow/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export PROJ_HOME=/home/logic/_workspace/LYNKED_QA_project
+export DOCS_DIR=$PROJ_HOME/docs
+export FLOWS_DIR=$DOCS_DIR/flows
+export USER_FLOW=$FLOWS_DIR/user_flow
+export PATH=$PATH:$USER_FLOW/node_modules/.bin
+
+# mmdc -i happy_flow_1.mmd -o happy_flow_1.png
+# mmdc -i happy_flow_1.mmd -o happy_flow_1.png
+
+# npm install
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-1/happy-flow-1.mmd -o $FLOWS_DIR/user_flow/happyflow-1/happy-flow-1.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-2/happy-flow-2.mmd -o $FLOWS_DIR/user_flow/happyflow-2/happy-flow-2.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-3/happy-flow-3.mmd -o $FLOWS_DIR/user_flow/happyflow-3/happy-flow-3.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-4/happy-flow-4.mmd -o $FLOWS_DIR/user_flow/happyflow-4/happy-flow-4.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-5/happy-flow-5.mmd -o $FLOWS_DIR/user_flow/happyflow-5/happy-flow-5.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-6/happy-flow-6.mmd -o $FLOWS_DIR/user_flow/happyflow-6/happy-flow-6.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-7/happy-flow-7.mmd -o $FLOWS_DIR/user_flow/happyflow-7/happy-flow-7.png &
+
+mmdc -i $FLOWS_DIR/user_flow/happyflow-8/happy-flow-8.mmd -o $FLOWS_DIR/user_flow/happyflow-8/happy-flow-8.png &
+
+wait
