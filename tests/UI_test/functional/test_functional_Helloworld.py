30ebf114af713a48f70d368b480251df612dd27f
louiscklaw <louiscklaw@gmail.com>
2020-11-19 20:37:57 +0800

    update and test,

--- tests/UI_test/functional/test_functional_Helloworld.py
+++ tests/UI_test/functional/test_functional_Helloworld.py
@@ -8,6 +8,7 @@ TEST_LIB_DIR=os.path.abspath(TEST_ROOT+'/lib')
 LIB_PO_DIR=os.path.abspath(TEST_LIB_DIR+'/pages')
 LIB_ELE_DIR=os.path.abspath(TEST_LIB_DIR+'/elements')
 LIB_CONFIG_DIR=os.path.abspath(TEST_LIB_DIR+'/configs')
+LIB_ASSERT_DIR=os.path.abspath(TEST_LIB_DIR+'/asserts')
 
 sys.path.append(UI_TEST_DIR)
 sys.path.append(TEST_ROOT)
@@ -15,11 +16,13 @@ sys.path.append(TEST_LIB_DIR)
 sys.path.append(LIB_PO_DIR)
 sys.path.append(LIB_ELE_DIR)
 sys.path.append(LIB_CONFIG_DIR)
+sys.path.append(LIB_ASSERT_DIR)
 
 from lib_helloworld import lib_helloworld
 from po_helloworld import po_helloworld
 from ele_helloworld import ele_helloworld
 from config_helloworld import config_helloworld
+from assert_helloworld import assert_helloworld
 
 def test_lib_directory():
   pprint(TEST_ROOT)
@@ -28,6 +31,7 @@ def test_lib_directory():
   assert 'po_helloworld helloworld'==po_helloworld()
   assert 'ele_helloworld helloworld'==ele_helloworld()
   assert 'config_helloworld helloworld'==config_helloworld()
+  assert 'assert_helloworld helloworld'==assert_helloworld()
 
   # pprint(TEST_ROOT)
   # pprint("helloworld_happyflow_1")
