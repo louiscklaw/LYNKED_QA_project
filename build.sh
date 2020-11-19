945749df1e64352bcf1e0fa7f29a76db9f62c648
louiscklaw <louiscklaw@gmail.com>
2020-11-19 20:29:16 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ mkdir -p reports/UI_test/functional/test_happyflow_1/result
 # pipenv sync
 
 # pipenv run pytest --html=reports/regression/report.html tests/UI_test/regression
-# pipenv run pytest --html=reports/functional/report.html tests/UI_test/functional
+pipenv run pytest --html=reports/functional/report.html tests/UI_test/functional
 # pipenv run pytest --html=reports/new_feature/report.html tests/UI_test/new_feature
 
 # pipenv run pytest tests/self_test
