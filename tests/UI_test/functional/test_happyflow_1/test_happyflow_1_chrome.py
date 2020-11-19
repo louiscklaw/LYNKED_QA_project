6649acc99c78f2defb368ffc052b3a9d162dd793
louiscklaw <louiscklaw@gmail.com>
2020-11-19 20:43:07 +0800

    update and test,

--- tests/UI_test/functional/test_happyflow_1/test_happyflow_1_chrome.py
+++ tests/UI_test/functional/test_happyflow_1/test_happyflow_1_chrome.py
@@ -12,18 +12,24 @@ LIB_PO_DIR=os.path.abspath(LIB_DIR+'/pages')
 LIB_ELE_DIR=os.path.abspath(LIB_DIR+'./elements')
 LANG_DIR=os.path.abspath(TEST_ROOT+'/lang')
 LIB_CONFIG_DIR=os.path.abspath(LIB_DIR+'/configs')
+LIB_ASSERT_DIR=os.path.abspath(LIB_DIR+'/asserts')
 
 sys.path.append(LIB_DIR)
 sys.path.append(LIB_PO_DIR)
 sys.path.append(LANG_DIR)
 sys.path.append(LIB_CONFIG_DIR)
+sys.path.append(LIB_ASSERT_DIR)
 
 from lib_helloworld import lib_helloworld
 from po_helloworld import po_helloworld
+from assert_image import assertSameImage
 from jp import *
 
 SELENIUM_HUB_HOST='localhost'
 FOOD_PAGE='http://menymeny.com/food/%E3%82%84%E3%81%8D%E3%81%A8%E3%82%8A/'
+
+ACTUAL_SCREENSHOT='reports/UI_test/functional/test_happyflow_1/result/first_time_landing_chrome.png'
+EXPECTED_SCREENSHOT='tests/UI_test/functional/test_happyflow_1/expected_result/first_time_landing_chrome.png'
 
 def test_happyflow_1_chrome():
   selenium_url = 'http://{}:4444/wd/hub'.format(SELENIUM_HUB_HOST)
@@ -38,7 +44,9 @@ def test_happyflow_1_chrome():
 
   browser.get(FOOD_PAGE)
 
-  browser.save_screenshot('reports/UI_test/functional/test_happyflow_1/result/first_time_landing_chrome.png')
+  browser.save_screenshot(ACTUAL_SCREENSHOT)
+
+  assertSameImage(EXPECTED_SCREENSHOT, ACTUAL_SCREENSHOT, 'first time landing test on chrome failed')
 
   browser.quit()
   sleep(30)
