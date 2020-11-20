75b927a31cc28e054c8bdc8413834bfe1ac27562
louiscklaw <louiscklaw@gmail.com>
2020-11-20 15:03:37 +0800

    update,

--- tests/fill_value_to_google.py
+++ tests/fill_value_to_google.py
@@ -3,16 +3,20 @@ import gspread
 from datetime import datetime
 from pprint import pprint
 import json
-
+import string
+from time import sleep
 import pytz
 
 TXT_RESULT_NOT_FOUND='ASK LOUIS'
+SHEET_TO_FILL="[result] E-Order Smoke Test Plan"
+REPORT_JSON_FILEPATH='./.report.json'
+
 
 gc = gspread.service_account()
 
-sh = gc.open("Example spreadsheet")
+sh = gc.open(SHEET_TO_FILL)
 
-in_json = ''.join(open('./.report.json','r').readlines())
+in_json = ''.join(open(REPORT_JSON_FILEPATH,'r').readlines())
 raw_result_json = json.loads(in_json)
 
 
@@ -24,17 +28,6 @@ def parseToTestIdAndResult():
     test_id_and_result_json[test_id]=test_result
   return test_id_and_result_json
 
-# result_json = {
-#   'TID_FAIL01': 'failed',
-#   'TID_FAIL02': 'failed',
-#   'TID_FAIL03': 'failed',
-#   'TID_PASS01': 'passed',
-#   'TID_PASS02': 'passed',
-#   'TID_PASS03': 'passed',
-#   'TID_PASS04': 'passed',
-#   'TID_PASS05': 'passed'
-#   }
-
 def fillNewDate(column_to_fill):
   datetime_now = datetime.now()
   # string_to_fill=datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -42,7 +35,7 @@ def fillNewDate(column_to_fill):
   d_aware = timezone.localize(  datetime_now)
   string_to_fill=d_aware.strftime('%Y-%m-%d %H:%M')
 
-  sh.sheet1.update_cell(1, column_to_fill, string_to_fill)
+  sh.sheet1.update_cell(ROW_DATETIME, column_to_fill, string_to_fill)
 
 def findEmptyCellInRow(row=1):
   for i in range(2,1000):
@@ -51,25 +44,57 @@ def findEmptyCellInRow(row=1):
       return i
 
 test_id_and_result_json=parseToTestIdAndResult()
+# test_id_and_result_json = {
+#   'TID_001': 'failed',
+#   'TID_002': 'failed',
+#   'TID_003': 'failed',
+#   }
 
-NEW_COLUMN_NUM=findEmptyCellInRow()
+ROW_DATETIME=4
+NEW_COLUMN_NUM=findEmptyCellInRow(ROW_DATETIME)
 
 fillNewDate(NEW_COLUMN_NUM)
 
-for i in range(2,150):
-  test_id_on_sheet=sh.sheet1.cell(i, 1).value
+COL_TID_NAME=14
+count_to_exit=0
+exit_when_larger_than=4
 
-  if test_id_on_sheet=='':
-    print("empty found, update done ?")
-    break
+def checkIfNeedToExit(count_to_exit, exit_when_larger_than):
+    if count_to_exit > exit_when_larger_than:
+      print('count to exit {} > {}, exitting'.format(count_to_exit ,exit_when_larger_than))
+      sys.exit()
+
+# vertical scanning
+# google limit read rate applied
+# https://gspread.readthedocs.io/en/latest/user-guide.html
+for i in range(5,150):
+  sleep(0.2)
+  test_id_on_sheet=sh.sheet1.cell(i, COL_TID_NAME).value
+
+  # if test_id_on_sheet=='':
+  #   print("empty found, update done ?")
+  #   break
 
   try:
+
     test_id_on_sheet=test_id_on_sheet.upper()
     test_result = test_id_and_result_json[test_id_on_sheet].upper()
     # sh.sheet1.update_cell(row,  column, testname)
     print('test_id_on_sheet {} found on row {}, filling result {} on col {}'.format(test_id_on_sheet, i, test_result, NEW_COLUMN_NUM))
     sh.sheet1.update_cell(i, NEW_COLUMN_NUM, test_result)
 
+    # value found, reset count_to_exit
+    count_to_exit=0
+
   except Exception as e:
-    print('test id not found, test id in spread sheet is {}'.format(test_id_on_sheet))
-    sh.sheet1.update_cell(i, NEW_COLUMN_NUM, TXT_RESULT_NOT_FOUND)
+    # currently either empty cell or cell TID not found
+    if test_id_on_sheet=='':
+      count_to_exit +=1
+      print('cell is empty, raise count to exit +1, current count {}'.format(count_to_exit))
+      checkIfNeedToExit(count_to_exit, exit_when_larger_than)
+
+    else:
+      print('test id not found, test id in spread sheet is {}'.format(test_id_on_sheet))
+      sh.sheet1.update_cell(i, NEW_COLUMN_NUM, TXT_RESULT_NOT_FOUND)
+  finally:
+    checkIfNeedToExit(count_to_exit, exit_when_larger_than)
