8c60de8abde0bd8ca4f57170a19896eaad4b656f
louiscklaw <louiscklaw@gmail.com>
2020-11-28 16:41:54 +0800

    add support lib,

---
+++ tests/lib/steps/check_TID_009.py
@@ -0,0 +1,23 @@
+import line_up_page
+import food_menu
+import line_up_confirmation_dialogue
+import item_add_page
+import cart_page
+import table_assigned_dialogue
+
+from config import *
+from time import sleep
+from assert_check_point import assertCheckPoint
+
+def run_check(json_metadata, browser, food_item_idx=1):
+  TEST_ERR_MSG='It should direct to item add page'
+
+  assertCheckPoint(browser, 'TID_009_1', TEST_ERR_MSG)
+
+  food_menu_po = food_menu.Main(browser)
+  food_menu_po.tapFoodItemByIdx(food_item_idx)
+
+  assertCheckPoint(browser, 'TID_009_2', TEST_ERR_MSG)
+  # assertSameImage(expected_screenshot_path, actual_screenshot_path,0.1,  TEST_ERR_MSG)
+
+  json_metadata['TID_009'] = 'passed'
