15f707d9e95d21f4865a9f73a4ddcb653e7ace41
louiscklaw <louiscklaw@gmail.com>
2020-11-28 18:13:53 +0800

    update working test,

--- integration.sh
+++ integration.sh
@@ -11,11 +11,16 @@ mkdir -p reports/UI_test/functional/test_happyflow_1/result
 
 pipenv sync
 
-# pipenv run pytest --maxfail=999 --json-report  --html=reports/functional/report.html -x tests/UI_test/functional/smoke_test
+  # --workers 14 \
+pipenv run pytest \
+  --maxfail=999 \
+  --json-report  \
+  --html=reports/functional/report.html \
+  -x tests/UI_test/functional/smoke_test
 
 
-pipenv run pytest --maxfail=999 --json-report  --html=reports/functional/report.html -x tests/UI_test/functional/smoke_test/test_TID_004.py &
+# pipenv run pytest --maxfail=999 --json-report  --html=reports/functional/report.html -x tests/UI_test/functional/smoke_test/test_TID_004.py &
 
-pipenv run pytest --maxfail=999 --json-report  --html=reports/functional/report.html -x tests/UI_test/functional/smoke_test/test_TID_005.py &
+# pipenv run pytest --maxfail=999 --json-report  --html=reports/functional/report.html -x tests/UI_test/functional/smoke_test/test_TID_005.py &
 
-wait
+# wait
